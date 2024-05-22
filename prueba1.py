1e4deedcfb2ee4c71d37e4aa60dfba0b4c130655
RogerJackD <1511638@senati.pe>
2024-05-21 23:04:43 -0500

    prueba def

--- prueba1.py
+++ prueba1.py
@@ -1,1 +1,16 @@
-print("hola mundo")
+def crear_Contraseña_random(num):
+    chars = "adfsegihj"
+    num_entero = str(num)
+    num = int (num_entero[0])
+    c1= num - 2
+    c2= num 
+    c3= num - 5
+    contraseña = f"{chars[c1]}{chars[c2]}{chars[c3]}{num*2}"
+    return contraseña,num 
+#desempaquetar
+password,primer_numero = crear_Contraseña_random("5")  
+
+print(f"tu primera contra es {password}")
+print(f"el numero usado fue {primer_numero}")
+
+#sga10
